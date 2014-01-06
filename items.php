ae1bbfc8b05f8bfb78876c97573c0c54faad7ba5
priyamalani <>
2014-01-06 14:31:47 -0500

    moving featured side to includes

--- items.php
+++ items.php
@@ -315,64 +315,7 @@
                   
                   <!-- Sidebar items (featured items)-->
 
-                   <div class="sidebar-items">
-
-                     <h5>Featured Items</h5>
-
-                     <!-- Item #1 -->
-                     <div class="sitem">
-                       <div class="onethree-left">
-                         <!-- Image -->
-                         <a href="single-item.php"><img src="files/2.png" alt="" class="img-responsive"></a>
-                       </div>
-                       <div class="onethree-right">
-                         <!-- Title -->
-                         <a href="single-item.php">HTC One V</a>
-                         <!-- Para -->
-                         <p>Aenean ullamcorper justo tincidunt justo aliquet.</p>
-                         <!-- Price -->
-                         <p class="bold">$199</p>
-                       </div>
-                       <div class="clearfix"></div>
-                     </div>
-
-                     <div class="sitem">
-                       <div class="onethree-left">
-                         <a href="single-item.php"><img src="files/3.png" alt="" class="img-responsive"></a>
-                       </div>
-                       <div class="onethree-right">
-                         <a href="single-item.php">Sony One V</a>
-                         <p>Aenean ullamcorper justo tincidunt justo aliquet.</p>
-                         <p class="bold">$399</p>
-                       </div>
-                       <div class="clearfix"></div>
-                     </div>
-
-                     <div class="sitem">
-                       <div class="onethree-left">
-                         <a href="single-item.php"><img src="files/4.png" alt="" class="img-responsive"></a>
-                       </div>
-                       <div class="onethree-right">
-                         <a href="single-item.php">Nokia One V</a>
-                         <p>Aenean ullamcorper justo tincidunt justo aliquet.</p>
-                         <p class="bold">$159</p>
-                       </div>
-                       <div class="clearfix"></div>
-                     </div>
-
-                     <div class="sitem">
-                       <div class="onethree-left">
-                         <a href="single-item.php"><img src="files/5.png" alt="" class="img-responsive"></a>
-                       </div>
-                       <div class="onethree-right">
-                         <a href="single-item.php">Samsung One V</a>
-                         <p>Aenean ullamcorper justo tincidunt justo aliquet.</p>
-                         <p class="bold">$299</p>
-                       </div>
-                       <div class="clearfix"></div>
-                     </div>
-                                             
-                   </div>
+                   <?php include 'includes/featuredSidebar.php'; ?>
                </div>
             </div>
             
