1e74695fe9a66599fc4a81039f281340e049feff
priyamalani <>
2014-01-06 13:53:30 -0500

    changing arrow

--- items.php
+++ items.php
@@ -260,13 +260,13 @@
                         <div class="col-md-12">
                                     <!-- Pagination -->
                                     <ul class="pagination">
-                                      <li><a href="#">�</a></li>
+                                      <li><a href="#"><i class="icon-caret-left"></i></a></li>
                                       <li><a href="#">1</a></li>
                                       <li><a href="#">2</a></li>
                                       <li><a href="#">3</a></li>
                                       <li><a href="#">4</a></li>
                                       <li><a href="#">5</a></li>
-                                      <li><a href="#">�</a></li>
+                                      <li><a href="#"><i class="icon-caret-right"></i></a></li>
                                     </ul> 
                         </div>
                      </div>
