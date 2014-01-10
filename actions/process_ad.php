bdaeb44ed0b44b99972d44ae62030f67c85304a2
priyamalani <>
2014-01-10 13:02:50 -0500

    saving correct category

--- actions/process_ad.php
+++ actions/process_ad.php
@@ -20,10 +20,10 @@ if (isset($_POST['name'], $_POST['sellingPrice'],$_POST['category'],$_POST['imag
     $imageFile2 = $_POST['imageFile2'];
     $imageFile3 = $_POST['imageFile3'];
     $product_id = mt_rand();
-
+    
    if (empty($error_msg)) {
         $query = "INSERT INTO product (product_name, product_desc, actual_price, selling_price, category_id, return_policy, info_1, info_2, info_3, quantity, estimated_delivery, shipping_charges, more_details, product_id, members_id) VALUES" .
-        "('$name', '$description', '$actualPrice', '$sellingPrice', 1, '$returnPolicy', '$imageFile1', '$imageFile2', '$imageFile3', '$quantity', '$estimatedDelivery', '$shippingCharges', '$moreDetails', '$product_id', '$mem_id')"; 
+        "('$name', '$description', '$actualPrice', '$sellingPrice', '$category', '$returnPolicy', '$imageFile1', '$imageFile2', '$imageFile3', '$quantity', '$estimatedDelivery', '$shippingCharges', '$moreDetails', '$product_id', '$mem_id')"; 
         if (!mysql_query($query, $db_server)){
             header('Location: ../404.php');
         }
