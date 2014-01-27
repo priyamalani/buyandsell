eb746eaab5d36e4197cf8070a43768930098ff3a
priyamalani <>
2014-01-27 16:19:31 -0500

    wishlist already exists

--- actions/add_wishlist.php
+++ actions/add_wishlist.php
@@ -8,7 +8,15 @@ if (isset($_POST['members_id'], $_POST['productNo'])) {
     $product_no = $_POST['productNo'];
     $members_id = $_POST['members_id'];
     
-    if (empty($error_msg)) {
+    $query = "SELECT * FROM wishlist where product_no=".$product_no." and members_id=".$members_id;
+    $result = mysql_query($query);
+    if (!$result) die ("Database access failed: " . mysql_error());
+    $rows = mysql_num_rows($result);
+    if($rows>0){
+        header('Location: ../wishlist.php');
+    }
+    
+    elseif (empty($error_msg)) {
         $query = "INSERT INTO wishlist (product_no, members_id ) VALUES" .
         "('$product_no' ,'$members_id')";
         if (!mysql_query($query, $db_server)){
